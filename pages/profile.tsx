9ebd46a6caf3937e0d11e2b479b75e8cc8d694a3
Anton <62949848+icepaq@users.noreply.github.com>
2022-09-20 22:07:25 -0400

    profile template

---
+++ pages/profile.tsx
@@ -0,0 +1,38 @@
+import styles from '../styles/Settings.module.css'
+
+const Profile = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.wrapper}>
+                <div className={styles.title}>
+                    Hi Anton
+                </div>
+
+                <div className={styles.section}>
+                    <div className={styles.sectionTitle}>
+                        Update Password
+                    </div>
+                    <input type="password" placeholder="Current Password" className={styles.input} /> <br />
+                    <input type="password" placeholder="New Password" className={styles.input} /> <br />
+                    <input type="password" placeholder="Confirm New Password" className={styles.input} /> <br />
+                    <div className={styles.button}>
+                        Update Password
+                    </div>
+                </div>
+                <div className={styles.section}>
+                    <div className={styles.sectionTitle}>
+                        My Pages
+                    </div>
+                    <div className={styles.page}>
+                        <div className={styles.pageTitle}>/@anton</div>
+                    </div>
+                    <div className={styles.button}>
+                        New Page
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile
