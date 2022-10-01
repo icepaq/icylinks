f2625e548ea092665645368650cab3057249a7f9
Matt Manna <MattManna0@gmail.com>
2022-09-30 21:31:27 -0400

    lmao

---
+++ pages/404.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import styles from '../styles/Error.module.css'
+
+export default function FourOhFour() {
+  return <>
+    <div className={styles.background}></div>
+      <img className={styles.errorImage} src="../error image.png"></img>
+      <a className={styles.notFound}>Oops!</a>
+      <a className={styles.description}>The page you are looking for doesn't exist.</a>
+      <Link href="/">
+        <a className={styles.link}>
+          Go Back Home
+        </a>
+      </Link>
+  </>
+}
