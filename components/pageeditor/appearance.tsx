9f9966fa3c89564b4fd49b685b45906a9f410dc8
Anton <62949848+icepaq@users.noreply.github.com>
2022-10-14 21:36:41 -0400

    format appearance.tsx

--- components/pageeditor/appearance.tsx
+++ components/pageeditor/appearance.tsx
@@ -1,139 +1,193 @@
-import styles from '../../styles/Edit.module.css'
-import Image from 'next/image';
-import { useEffect } from 'react';
+import styles from "../../styles/Edit.module.css";
+import Image from "next/image";
+import { useEffect } from "react";
 
+const Appearance = ({
+  id,
+  setID,
+  setbackgroundCSS,
+  setSelectedImage,
+  setBackgroundType,
+  backgroundType,
+  setSelectedBackgroundImage,
+  setBackgroundColor,
+  backgroundColor,
+  profile,
+  setTitle,
+  setDescription,
+  selectedImage,
+}: any) => {
+  const changeID = async (e: any) => {
+    setID(e.target.value);
+  };
 
-const Appearance = ({id, setID, setbackgroundCSS, setSelectedImage, setBackgroundType, 
-                     backgroundType, setSelectedBackgroundImage, setBackgroundColor, 
-                     backgroundColor, profile, setTitle, setDescription, selectedImage}: any) => {
-    const changeID = async (e: any) => {
-        setID(e.target.value);
+  useEffect(() => {
+    console.log(selectedImage);
+  });
+
+  const updateBackground = (type: string) => {
+    if (type === "color") {
+      setbackgroundCSS({ backgroundColor: backgroundColor });
+    } else {
+      setbackgroundCSS({
+        backgroundImage: 'url("/background.png")',
+        backgroundSize: "100% 100%",
+      });
     }
+  };
 
-    useEffect(() => {
-        console.log(selectedImage)
-    })
+  const updateImage = async (e: any) => {
+    const params = new FormData();
+    params.append("file", e);
+    const r = await fetch("/api/upload", {
+      method: "POST",
+      body: params,
+    }).then((res) => res.json());
 
-    const updateBackground = (type: string) => {
-        if (type === 'color') {
-            setbackgroundCSS({backgroundColor: backgroundColor});
-        } else {
-            setbackgroundCSS({backgroundImage: 'url("/background.png")', backgroundSize: '100% 100%'});
-        }
-    }
+    setSelectedImage(r.data);
+  };
 
-    const updateImage = async (e: any) => {
-        const params = new FormData();
-        params.append('file', e);
-        const r = await fetch('/api/upload', {
-            method: 'POST',
-            body: params
-        }).then(res => res.json());
+  const updateBackgroundImage = async (e: any) => {
+    const params = new FormData();
+    params.append("file", e);
+    const r = await fetch("/api/upload", {
+      method: "POST",
+      body: params,
+    }).then((res) => res.json());
 
-        setSelectedImage(r.data);
-    }
+    setSelectedBackgroundImage(r.data);
 
-    const updateBackgroundImage = async (e: any) => {
-        const params = new FormData();
-        params.append('file', e);
-        const r = await fetch('/api/upload', {
-            method: 'POST',
-            body: params
-        }).then(res => res.json());
+    setbackgroundCSS({
+      backgroundImage: 'url("' + r.data + '")',
+      backgroundSize: "100% 100%",
+    });
+  };
 
-        setSelectedBackgroundImage(r.data);
+  return (
+    <>
+      <div className={styles.picture}>
+        <div className={styles.sectionTitle}>Profile</div>
+        <div className={styles.pictureContainer}>
+          <span className={styles.circleImage}>
+            {selectedImage ? (
+              <Image src={selectedImage} width={100} height={100} />
+            ) : null}
+          </span>
+          <span className={styles.photoActions}>
+            <div>
+              <label className={styles.greyButton}>
+                <input
+                  className={styles.greyButton}
+                  type="file"
+                  name="myImage"
+                  onChange={(event: any) => {
+                    updateImage(event.target.files[0]);
+                  }}
+                />
+                New Image
+              </label>
+            </div>
+            <div className={styles.greyButton}>Remove Image</div>
+          </span>
+        </div>
+      </div>
 
-        setbackgroundCSS({backgroundImage: 'url("' + r.data + '")', backgroundSize: '100% 100%'});
-    }
+      <div className={styles.header}>
+        <input
+          className={styles.titleInput}
+          type={"text"}
+          placeholder={"Title"}
+          defaultValue={profile.title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+      <div className={styles.linkInput}>
+        <input
+          className={styles.titleInput}
+          type={"text"}
+          placeholder={"Title"}
+          defaultValue={id}
+          onChange={changeID}
+        />
+      </div>
+      <textarea
+        className={styles.textarea}
+        onChange={(e) => setDescription(e.target.value)}
+        defaultValue={profile.description}
+      />
 
-    return (
-        <>
-                
-                <div className={styles.picture}>
-                    <div className={styles.sectionTitle}>
-                        Profile
-                    </div>
-                    <div className={styles.pictureContainer}>
-                        <span className={styles.circleImage}>
-                            {selectedImage ? <Image src={selectedImage} width={100} height={100} /> : null}
-                        </span>
-                        <span className={styles.photoActions}>
-                            <div>
-                                <label className={styles.greyButton} >
-                                    <input
-                                        className={styles.greyButton} type="file" name="myImage" onChange={(event: any) => {
-                                            updateImage(event.target.files[0]);
-                                        }}
-                                    />
-                                New Image
-                                </label>
-                            </div>
-                            <div className={styles.greyButton}>
-                                Remove Image
-                            </div>
-                        </span>
-                        
-                    </div>
-                    
-                    
-                </div>
+      <div className={styles.sectionTitle}>Background</div>
+      <div className={styles.backgroundSelect}>
+        <span className={styles.backgroundSelector}>
+          <input
+            className={styles.input}
+            type="radio"
+            id="html"
+            name="fav_language"
+            value="color"
+            onClick={(e) => {
+              setBackgroundType("color");
+              updateBackground("color");
+            }}
+          />
+          <label htmlFor="color">Color</label>
+        </span>
 
-                <div className={styles.header}>
-                    <input className={styles.titleInput} type={'text'} placeholder={'Title'} defaultValue={profile.title} onChange={(e) => setTitle(e.target.value)} />
-                </div>
-                <div className={styles.linkInput} >
-                    <input className={styles.titleInput} type={'text'} placeholder={'Title'} defaultValue={id} onChange={changeID} />
-                </div>
-                <textarea className={styles.textarea} onChange={(e) => setDescription(e.target.value)} defaultValue={profile.description} />
+        <span className={styles.backgroundSelector}>
+          <input
+            className={styles.input}
+            type="radio"
+            id="html"
+            name="fav_language"
+            value="image"
+            onClick={(e) => {
+              setBackgroundType("image");
+              updateBackground("image");
+            }}
+          />
+          <label htmlFor="image">Image</label>
+        </span>
+      </div>
 
-                <div className={styles.sectionTitle}>
-                    Background
-                </div>
-                <div className={styles.backgroundSelect}>
-                    <span className={styles.backgroundSelector}>
-                        <input className={styles.input} type="radio" id="html" name="fav_language" value="color" onClick={(e) => {setBackgroundType('color'); updateBackground('color')}} />
-                        <label htmlFor="color">Color</label>
-                    </span>
-                    
-                    <span className={styles.backgroundSelector}>
+      {backgroundType === "color" ? (
+        <label>
+          <input
+            className={styles.colorInput}
+            type="color"
+            id="colorpicker"
+            value="#0000ff"
+            onChange={(e) => {
+              setBackgroundColor(e.target.value);
+              updateBackground("color");
+            }}
+          />
+          <div className={styles.color}></div>
+        </label>
+      ) : (
+        <input
+          type="file"
+          name="myImage"
+          onChange={(event: any) => {
+            updateBackgroundImage(event.target.files[0]);
+          }}
+        />
+      )}
 
-                    <input className={styles.input} type="radio" id="html" name="fav_language" value="image" onClick={(e) => {setBackgroundType('image'); updateBackground('image')}} />
-                    <label htmlFor="image">Image</label>
-                    </span>
+      <div>
+        <label className={styles.greyButton}>
+          <input
+            className={styles.greyButton}
+            type="file"
+            name="myImage"
+            onChange={(event: any) => {
+              updateImage(event.target.files[0]);
+            }}
+          />
+          New Image
+        </label>
+      </div>
+    </>
+  );
+};
 
-                </div>
-
-                {
-                    backgroundType === 'color' ? (
-                        <label >
-                                <input className={styles.colorInput} type="color" id="colorpicker" value="#0000ff" onChange={(e) => {setBackgroundColor(e.target.value); updateBackground('color')} }/>
-                                <div className={styles.color}></div>
-                            </label>
-                        ) : (
-                        <input
-                            type="file"
-                            name="myImage"
-                            onChange={(event: any) => {
-                                updateBackgroundImage(event.target.files[0]);
-                            }}
-                        />
-                    )
-
-                }
-
-                <div>
-                    <label className={styles.greyButton} >
-                        <input
-                            className={styles.greyButton} type="file" name="myImage" onChange={(event: any) => {
-                                updateImage(event.target.files[0]);
-                            }}
-                        />
-                    New Image
-                    </label>
-                </div>
-                
-        </>
-    )
-}
-
-export default Appearance
+export default Appearance;
